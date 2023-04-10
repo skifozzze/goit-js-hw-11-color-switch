7bec3178dcb07417778aa26ebdb9a8753de217a3
skifozzze <skifozzze@gmail.com>
2023-04-10 14:51:22 +0300

    upd

--- index.js
+++ index.js
@@ -19,22 +19,18 @@ refs.stopBtnEl.addEventListener('click', onStopBtnClick);
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-var isActive = false;
+
 var intervalId = null;
 
 function onStartBtnClick() {
-  if (isActive) {
-    return;
-  }
-
-  isActive = true;
   intervalId = setInterval(() => {
     const randomNum = randomIntegerFromInterval(0, colors.length - 1);
     refs.bodyEl.style.backgroundColor = colors[randomNum];
   }, 1000);
+  refs.startBtnEl.disabled = true;
 }
 
 function onStopBtnClick() {
   clearInterval(intervalId);
-  isActive = false;
+  refs.startBtnEl.disabled = false;
 }
